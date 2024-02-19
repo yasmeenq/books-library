1528eb26d0d4e2bffcda4d96349d19d850188a09
Yasmeen Qawasmy <71906328+yasmeenq@users.noreply.github.com>
2024-02-19 22:46:18 +0200

    Update library.py

--- library.py
+++ library.py
@@ -1,6 +1,3 @@
-#Build a library management system using dictionaries 
-#where the keys are book titles and the values are dictionaries 
-#containing information about each book (e.g., author, genre, publication year).
 
 library = {
             "the night circus":{"author": "Erin Morgenstern",
